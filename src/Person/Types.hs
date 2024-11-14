8725da1cdab9edb63e0fd736605ee8d08c2bedd7
Eric "सन्तोष" Zoerner <eric.zoerner@proton.me>
2024-11-13 16:01:52 -0800

    Implement basic validation for phone and ssn

--- src/Person/Types.hs
+++ src/Person/Types.hs
@@ -60,12 +60,16 @@ data PersonError
 -- | Construct a valid US PhoneNumber, or return Failure with a list of PersonErrors
 -- this should decompose the number into area code, exchange, and number and
 -- validate each part
--- Note this is stubbed out and not yet implemented
+-- This is stubbed out and only checks to make sure the number has ten digits total.
+-- A full implemention should validate the area code and exchange as well.
 mkPhoneNumber :: Int -> Validation [PersonError] PhoneNumber
-mkPhoneNumber _ = Success $ PhoneNumber 123 555 1212
+mkPhoneNumber num
+  | length (digits 10 num) == 10 = Success $ PhoneNumber 123 555 1212
+  | otherwise = Failure [InvalidPhoneNumber]
 
 -- | Construct a valid Social Security Number
 -- A social security number must have 9 digits
--- Note this is stubbed out and not yet implemented
 mkSocialSecurityNumber :: Int -> Validation [PersonError] SocialSecurityNumber
-mkSocialSecurityNumber num = Success $ SocialSecurityNumber num
+mkSocialSecurityNumber num
+  | length (digits 10 num) == 9 = Success $ SocialSecurityNumber num
+  | otherwise = Failure [InvalidSSN]
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,4 +5,4 @@ import Person
 main :: IO ()
 main = do
   print . validatePerson $ EditPerson "John" "Doe" 123456789 Single 1234567890
-  print . validatePerson $ EditPerson "" "" 123456789 Married 12345678
+  print . validatePerson $ EditPerson "" "" 1234 Married 12345
