a0de6093f1f3f1394595191d9bdc33babb851ad7
Eric "सन्तोष" Zoerner <eric.zoerner@proton.me>
2024-11-13 16:42:26 -0800

    Added evals to comment in test

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,6 +2,16 @@
 
 import Person
 
+{-
+>>> validatePerson $ EditPerson "John" "Doe" 123456789 Single 1234567890
+Success (Person {firstName = "John", lastName = "Doe", ssn = 123-45-6789, martialStatus = Single, usPhoneNumber = (123) 456-7890})
+
+>>> validatePerson $ EditPerson "" "" 1234 Married 12345
+Failure [EmptyFirstName,EmptyLastName,InvalidSSN,InvalidPhoneNumber]
+
+>>> validatePerson $ EditPerson "John" "Doe" (-123456789) Married (-1234567890)
+Failure [InvalidSSN,InvalidPhoneNumber]
+-}
 main :: IO ()
 main = do
   print . validatePerson $ EditPerson "John" "Doe" 123456789 Single 1234567890
